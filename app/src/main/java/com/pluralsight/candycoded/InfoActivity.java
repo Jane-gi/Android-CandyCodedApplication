6b975ddac733c7d8f7fb1ce167d9fa563782a5db
Janet Adjey <janetadjey7@gmail.com>
2020-10-01 09:41:34 +0100

    phone activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -28,10 +28,6 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
-
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
     public void createMapIntent(View view) {
         Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
@@ -40,4 +36,14 @@ public class InfoActivity extends AppCompatActivity {
             startActivity(mapIntent);
         }
     }
-}
+
+    // ***
+    // TODO - Task 3 - Launch the Phone Activity
+    // ***
+
+    public void createPhoneIntent(View view){
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse( "tel:0123456789"));
+        startActivity(callIntent);
+    }
+   }
